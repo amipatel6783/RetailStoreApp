00b020b881d6be7060a4d79cc476f9832909783b
amipatel6783 <65390700+amipatel6783@users.noreply.github.com>
2021-09-02 14:35:13 +0530

    updated ordercontroller.java
    added static data for testing

--- src/main/java/com/retailstore/controller/OrderController.java
+++ src/main/java/com/retailstore/controller/OrderController.java
@@ -1,15 +1,22 @@
 package com.retailstore.controller;
 
-import java.time.LocalDate;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.retailstore.dto.OrderDTO;
+import com.retailstore.enumeration.ProductTypes;
+import com.retailstore.enumeration.UserTypes;
 import com.retailstore.exception.ApiRequestException;
 import com.retailstore.model.Order;
+import com.retailstore.model.Product;
+import com.retailstore.model.User;
 import com.retailstore.service.OrderService;
 
 @RestController
@@ -20,8 +27,16 @@ public class OrderController {
 	private OrderService orderService;
 	
 	@PostMapping("/addNew")
-	public Order saveOrder(@RequestBody OrderDTO orderDTO) {
+	public Order saveOrder() throws ParseException {
 		
+		Date custJoinDate = new SimpleDateFormat("yyyy-MM-dd").parse("2017-09-02");  
+		User user = new User(1,"patelami", UserTypes.CUSTOMER, custJoinDate);
+		List<Product> products = Arrays.asList(
+				new Product(101, "groceryProduct", 125.00, ProductTypes.GROCERIES),
+				new Product(102, "groceryProduct", 235.00, ProductTypes.GROCERIES),
+				new Product(201, "housewareProduct", 95.00, ProductTypes.HOUSEWARES),
+				new Product(301, "clothingProduct", 575.00, ProductTypes.CLOTHING));
+		OrderDTO orderDTO = new OrderDTO(user, products);
 		Order savedOrder = this.orderService.giveOrder(orderDTO);
 		if(savedOrder != null) {
 			return savedOrder;
